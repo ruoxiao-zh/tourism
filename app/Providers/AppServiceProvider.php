ed6d599ff231a88d7463a6249b3d51462f1d24f8
Ellison <ruoxiaozh@163.com>
2018-08-05 10:17:29 +0800

    添加微信支付库

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Monolog\Logger;
+use Yansongda\Pay\Pay;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -28,6 +30,17 @@ class AppServiceProvider extends ServiceProvider
         });
         \API::error(function (\Illuminate\Auth\Access\AuthorizationException $exception) {
             abort(403, $exception->getMessage());
+        });
+        // 微信支付
+        $this->app->singleton('wechat_pay', function () {
+            $config = config('pay.wechat');
+            if (app()->environment() !== 'production') {
+                $config['log']['level'] = Logger::DEBUG;
+            } else {
+                $config['log']['level'] = Logger::WARNING;
+            }
+            // 调用 Yansongda\Pay 来创建一个微信支付对象
+            return Pay::wechat($config);
         });
     }
 }
---
+++ config/pay.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'wechat' => [
+        'app_id'      => '',
+        'mch_id'      => '',
+        'key'         => '',
+        'cert_client' => '',
+        'cert_key'    => '',
+        'log'         => [
+            'file' => storage_path('logs/wechat_pay.log'),
+        ],
+    ],
+];
