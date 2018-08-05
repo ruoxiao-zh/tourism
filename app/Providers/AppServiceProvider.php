6ec4c343d5fd8834487258243976e39e523e3941
Ellison <ruoxiaozh@163.com>
2018-08-05 11:53:23 +0800

    测试微信支付

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -35,7 +35,7 @@ class AppServiceProvider extends ServiceProvider
         $this->app->singleton('wechat_pay', function () {
             $config = config('pay.wechat');
             // 微信支付回调地址
-            $config['notify_url'] = 'https://www.fljjcm.com/api/payment/wechat/notify';
+            // $config['notify_url'] = 'https://www.fljjcm.com/api/payment/wechat/notify';
             if (app()->environment() !== 'production') {
                 $config['log']['level'] = Logger::DEBUG;
             } else {
