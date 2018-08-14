52c30ce21c8e88e99f52c037dbf204d239eb6266
ellison <ruoxiaozh@163.com>
2018-08-14 10:27:04 +0800

    fixed bug

--- app/Http/Controllers/Api/UserPermissionController.php
+++ app/Http/Controllers/Api/UserPermissionController.php
@@ -12,20 +12,15 @@ class UserPermissionController extends Controller
 {
     public function store(UserPermissionRequest $request, UserPermission $userPermission)
     {
+	$userPermission->where('user_id', $request->user_id)->delete();
         $permission = json_decode($request->permission, true);
         foreach ($permission as $key => $value) {
-            $is_own = $userPermission->where([
+            $userPermission->insert([
                 'user_id'    => $request->user_id,
                 'permission' => $value['permission'],
-            ])->first();
-            if (!$is_own) {
-                $userPermission->insert([
-                    'user_id'    => $request->user_id,
-                    'permission' => $value['permission'],
-                    'created_at' => Carbon::now(),
-                    'updated_at' => Carbon::now(),
-                ]);
-            }
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ]);
         }
 
         $query = $userPermission->query();
